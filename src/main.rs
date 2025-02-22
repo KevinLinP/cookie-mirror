d64bb3b3182867f4dcf789410df54f4d48af057c
Kevin Lin <github@kevinlinp.org>
2025-02-22 08:35:34 -0800

    get and set_keys

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,39 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, cookie::Cookie};
+mod secrets;
 
-async fn hello_world() -> impl Responder {
-    HttpResponse::Ok().body("Hello World!")
+pub const COOKIE_NAME: &str = "auth_cookie";
+
+async fn get_keys(req: actix_web::HttpRequest) -> impl Responder {
+    // Check for the cookie
+    if let Some(cookie) = req.cookie(COOKIE_NAME) {
+        if cookie.value() == secrets::REQUIRED_COOKIE_VALUE {
+            HttpResponse::Ok().json(secrets::keys())
+        } else {
+            HttpResponse::Unauthorized().finish()
+        }
+    } else {
+        HttpResponse::Unauthorized().finish()
+    }
+}
+
+async fn set_auth_cookie(body: String) -> impl Responder {
+    // let cookie = Cookie::new(COOKIE_NAME, body);
+    let cookie = Cookie::build(COOKIE_NAME, body)
+    .secure(true)
+    .http_only(true)
+    .finish();
+
+    HttpResponse::Ok()
+        .cookie(cookie)
+        .finish()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().route("/", web::get().to(hello_world))
+        App::new()
+            .route("/set_auth_cookie", web::post().to(set_auth_cookie))
+            .route("/", web::get().to(get_keys))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
---
+++ src/secrets.template.rs
@@ -0,0 +1,9 @@
+use serde_json::Value;
+
+// pub const REQUIRED_COOKIE_VALUE: &str = "";
+
+// pub fn keys() -> Value {
+//     serde_json::json!({
+//         "message": "Hello World!",
+//     })
+// }
