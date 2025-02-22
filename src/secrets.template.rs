74fbb043faf04ac8e79da18bacd50d4dd86a22e1
Kevin Lin <github@kevinlinp.org>
2025-02-22 08:43:04 -0800

    tweak template

--- src/secrets.template.rs
+++ src/secrets.template.rs
@@ -1,9 +1,9 @@
 use serde_json::Value;
 
-// pub const REQUIRED_COOKIE_VALUE: &str = "";
+pub const REQUIRED_COOKIE_VALUE: &str = "secret_value";
 
-// pub fn keys() -> Value {
-//     serde_json::json!({
-//         "message": "Hello World!",
-//     })
-// }
+pub fn keys() -> Value {
+    serde_json::json!({
+        "abc": 123,
+    })
+}
