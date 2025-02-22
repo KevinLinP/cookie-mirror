30628efd2161d3c60328ca2685673ac7b6c2e453
Kevin Lin <github@kevinlinp.org>
2025-02-22 08:41:00 -0800

    tweak binding port

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ mod secrets;
 pub const COOKIE_NAME: &str = "auth_cookie";
 
 async fn get_keys(req: actix_web::HttpRequest) -> impl Responder {
-    // Check for the cookie
     if let Some(cookie) = req.cookie(COOKIE_NAME) {
         if cookie.value() == secrets::REQUIRED_COOKIE_VALUE {
             HttpResponse::Ok().json(secrets::keys())
@@ -17,7 +16,6 @@ async fn get_keys(req: actix_web::HttpRequest) -> impl Responder {
 }
 
 async fn set_auth_cookie(body: String) -> impl Responder {
-    // let cookie = Cookie::new(COOKIE_NAME, body);
     let cookie = Cookie::build(COOKIE_NAME, body)
     .secure(true)
     .http_only(true)
@@ -35,7 +33,7 @@ async fn main() -> std::io::Result<()> {
             .route("/set_auth_cookie", web::post().to(set_auth_cookie))
             .route("/", web::get().to(get_keys))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 317))? // Bind to all interfaces
     .run()
     .await
 }
